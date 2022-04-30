f1d91689bd2cca0d4786dd94317be2e44b677741
unknown <aka.mirov12@gmail.com>
2022-04-30 15:46:44 +0600

    add a

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
  const bGround = require("fcc-express-bground");
 
-bGround.log("Hellow World")
+bGround.log("Hello World")
 console.log("Hello World")
 
 module.exports = app;
