800765cc80250ec27f4bb3626a9d3d0d2b111f0d
unknown <aka.mirov12@gmail.com>
2022-05-01 18:28:00 +0600

    Add a json with uppercase

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const res = require('express/lib/response');
 var app = express();
-
+require('dotenv').config()
 console.log("Hello World")
 
 app.use("/public",express.static(__dirname +'/public'))
@@ -10,10 +10,19 @@ app.get('/',(req,res) =>{
     res.sendFile(__dirname + "/views/index.html")
   });
 
-app.get("/json",(req,res) => {
-    res.json({
-        "message":"Hello json"
-    })
-})
+// app.get("/json",(req,res) => {
+//     res.json({
+//         "message":"Hello json"
+//     })
+// })
 
+
+app.get('/json',(req,res)=>{
+    console.log(process.env.MESSAGE_STYLE) 
+    if(process.env.MESSAGE_STYLE === "uppercase"){
+        res.json({"message":"HELLO WORLD"})
+    }else {
+        res.json({"message":"Hello json"})
+    }
+})
 module.exports = app;
